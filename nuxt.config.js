6667b4138b30b5f9e89f3a7f4c5891adce40153a
Ismail Omodara <omodara145@gmail.com>
2020-09-24 08:51:40 +0100

    Completed homepage slider

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,55 +7,61 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'esphp-new',
+    title: 'NSHP - National Social Housing Programme',
     meta: [
       { charset: 'utf-8' },
       {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
       { name: 'viewport', content: 'width=device-width, initial-scale=1'},
       // The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags
 
-      {name: 'author', content: 'Eden Life Concierge Ltd'},
+      {name: 'author', content: 'National Social Housing Programme'},
       {name: 'theme-color', content: '#20B26D'},
-      {name: 'description', content: 'We\'ve created a food experience like nothing you\'ve ever seen. Be one of the first to try it out. Request a demo call today!'},
-      {name: 'keywords', content:'Eden Life in Nigeria, Eden Concierge in Nigeria, Eden Concierge, Eden Life, Improving quality of life, Quality of Life in Nigeria, Excellent service, Food service, ouredenlife, subscription plans, eden food delivery, best housework app, weekly meal delivery, food delivery Lagos, how to order food online in Nigeria, the best delivery service for food?, food delivery lekki, food delivery in Yaba, food delivery on the mainland'},
+      {name: 'description', content: ''},
+      {name: 'keywords', content:''},
 
       // Schema.org markup for Google+
-      {itemprop: 'name', content: 'Sign up for Early Access to Eden\'s New Food Subscription'},
-      {itemprop: 'description', content: 'We\'ve created a food experience like nothing you\'ve ever seen. Be one of the first to try it out. Request a demo call today!'},
-      {itemprop: 'image', content: 'https://choplife.ouredenlife.com/food-product-card.png'},
+      {itemprop: 'name', content: ''},
+      {itemprop: 'description', content: ''},
+      {itemprop: 'image', content: ''},
 
       // Twitter Card data
       {name: 'twitter:card', content: 'summary_large_image'},
-      {name: 'twitter:site', content: '@ouredenlife'},
-      {name: 'twitter:title', content: 'Sign up for Early Access to Eden\'s New Food Subscription'},
-      // {name: 'twitter:image', content: 'https://choplife.ouredenlife.com/edentwittercard.png?4362984378'},
-      {name:"twitter:url", content:"https://choplife.ouredenlife.com"},
-      {name: 'twitter:image', content: 'https://choplife.ouredenlife.com/food-product-card.png'},
-      {name: 'twitter:description', content: 'We\'ve created a food experience like nothing you\'ve ever seen. Be one of the first to try it out. Request a demo call today!'},
+      {name: 'twitter:site', content: '@nshp_ng'},
+      {name: 'twitter:title', content: ''},
+      {name:"twitter:url", content:""},
+      {name: 'twitter:image', content: ''},
+      {name: 'twitter:description', content: ''},
       {name: 'twitter:app:country', content: 'NG'},
-      {name:"twitter:creator", content:"@ouredenlife"},
-      {name:"twitter:domain", content:"@ouredenlife"},
+      {name:"twitter:creator", content:"@nshp_ng"},
+      {name:"twitter:domain", content:"@nshp_ng"},
       // Twitter summary card with large image must be at least 280x150px
-      {name: 'twitter:image:src', content: 'https://choplife.ouredenlife.com/food-product-card.png'},
-      // {name: 'twitter:image:src', content: 'https://choplife.ouredenlife.com/edentwittercard.png?4362984378'},
+      {name: 'twitter:image:src', content: ''},
 
       // Open Graph data
-      {property: 'og:title', content: 'Sign up for Early Access to Eden\'s New Food Subscription'},
-      {property: 'og:url', content: 'https://choplife.ouredenlife.com'},
-      {property: 'og:image', content: 'https://choplife.ouredenlife.com/food-product-card.png'},
-      {property: 'og:description', content: 'We\'ve created a food experience like nothing you\'ve ever seen. Be one of the first to try it out. Request a demo call today!'},
+      {property: 'og:title', content: ''},
+      {property: 'og:url', content: ''},
+      {property: 'og:image', content: ''},
+      {property: 'og:description', content: ''},
       {property: 'og:type', content: 'website'},
-      {property: 'og:site_name', content: 'Eden'},
+      {property: 'og:site_name', content: 'NSHP'},
 
       // Open Graph for WhatsApp
-      {property: 'og:title', content: 'Sign up for Early Access to Eden\'s New Food Subscription'},
-      {property: 'og:url', content: 'https://choplife.ouredenlife.com'},
-      {property: 'og:image', content: 'https://choplife.ouredenlife.com/food-product-card-whatsapp.png'},
-      {property: 'og:description', content: 'We\'ve created a food experience like nothing you\'ve ever seen. Be one of the first to try it out. Request a demo call today!'},
+      {property: 'og:title', content: ''},
+      {property: 'og:url', content: ''},
+      {property: 'og:image', content: ''},
+      {property: 'og:description', content: ''},
       {property: 'og:type', content: 'website'},
       {property: 'og:site_name', content: 'Eden'}
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+    link: [
+      { rel: 'icon', href: '/favicon.png' },
+      // {rel: 'icon', type: 'image/png', sizes: '144x144', href: './brand/platform-icons/android-144.png'},
+      // {rel: 'icon', type: 'image/png', sizes: '32x32', href: './brand/platform-icons/android-32.png'},
+      // {rel: 'apple-touch-icon', sizes: '180x180', href: './brand/platform-icons/ios-180.png'},
+      // {rel: 'apple-touch-icon', sizes: '120x120', href: './brand/platform-icons/ios-120.png'},
+      // {rel: 'apple-touch-icon', sizes: '152x152', href: './brand/platform-icons/ios-152.png'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap'}
+    ],
     script: [
       // Google Analytics
       {
@@ -87,6 +93,7 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     'element-ui/lib/theme-chalk/index.css',
+    '@/assets/css/variables.scss',
     '@/assets/css/main.scss',
     '@/assets/css/media.scss',
   ],
@@ -109,5 +116,16 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     transpile: [/^element-ui/],
+    extend(config, ctx) {
+      config.module.rules.push({
+        enforce: "pre",
+        test: /\.(js|vue)$/,
+        loader: "eslint-loader",
+        exclude: /(node_modules)/,
+        options: {
+          fix: true
+        }
+      })
+    }
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,379 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">esphp-new</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+  <header class="ms-header">
+    <div class="carousel-container">
+      <span class="prev" @click="prevMenu">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24.009"
+          height="16.003"
+          viewBox="0 0 24.009 16.003"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
+          <defs>
+            <style>
+              .pre {
+                fill: #fff;
+              }
+            </style>
+          </defs>
+          <path
+            class="pre"
+            d="M8.687.306A1.089,1.089,0,0,1,8.692,1.84L3.624,6.915H22.917a1.083,1.083,0,0,1,0,2.167H3.635L8.7,14.157a1.1,1.1,0,0,1-.008,1.533,1.08,1.08,0,0,1-1.526-.008L.3,8.766h0a1.216,1.216,0,0,1-.225-.342,1.034,1.034,0,0,1-.083-.418A1.086,1.086,0,0,1,.3,7.248L7.169.331A1.062,1.062,0,0,1,8.687.306Z"
+            transform="translate(0.008 0.001)"
+          />
+        </svg>
+      </span>
+      <div class="carousel">
+        <carousel
+          ref="slider"
+          :per-page="1"
+          :loop="true"
+          :autoplay="true"
+          :autoplay-timeout="5000"
+          :autoplay-hover-pause="false"
+          :pagination-padding="6"
+          :pagination-size="12"
+          pagination-active-color="#FCB320"
         >
-          GitHub
-        </a>
+          <slide
+            v-for="(slide, index) in slides"
+            :key="index"
+            :data-index="index"
+          >
+            <div class="slide-image">
+              <img :src="url(slide.slide)" alt="NSHP" />
+              <div class="overlay header-gradient"></div>
+            </div>
+            <div class="slide-content">
+              <h1>{{ slide.text }}</h1>
+            </div>
+          </slide>
+        </carousel>
       </div>
+      <span class="next" @click="nextMenu">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="16"
+          viewBox="0 0 24 16"
+        >
+          <defs>
+            <style>
+              .nex {
+                fill: #fff;
+              }
+            </style>
+          </defs>
+          <path
+            class="nex"
+            d="M15.305.306A1.089,1.089,0,0,0,15.3,1.84l5.068,5.075H1.075a1.083,1.083,0,0,0,0,2.167H20.357l-5.068,5.075a1.1,1.1,0,0,0,.008,1.533,1.08,1.08,0,0,0,1.526-.008l6.869-6.916h0a1.216,1.216,0,0,0,.225-.342A1.034,1.034,0,0,0,24,8.006a1.086,1.086,0,0,0-.308-.758L16.823.331A1.062,1.062,0,0,0,15.305.306Z"
+          />
+        </svg>
+      </span>
     </div>
-  </div>
+  </header>
 </template>
 
 <script>
-export default {}
+import { Carousel, Slide } from 'vue-carousel'
+
+export default {
+  name: 'Home',
+  components: {
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {
+      slides: [
+        {
+          text: 'Homes by Nigerians for Nigerians',
+          slide: '1',
+        },
+        {
+          text: 'Own a home for as low as ₦2million',
+          slide: '2',
+        },
+        {
+          text: 'Shikini money for beta house',
+          slide: '3',
+        },
+        {
+          text: 'Owo kekere fun ile to dara',
+          slide: '4',
+        },
+        {
+          text: 'Biyan kuɗi kaɗan don gida mai kyau',
+          slide: '5',
+        },
+        {
+          text: 'Kwụ ụgwọ obere ego maka ezigbo ụlọ',
+          slide: '6',
+        },
+        {
+          text: 'Beta work. Beta house. Beta life for evribodi',
+          slide: '2',
+        },
+      ],
+    }
+  },
+  mounted() {
+    document
+      .querySelectorAll('.VueCarousel-slide')[0]
+      .classList.add('VueCarousel-slide-active')
+  },
+  methods: {
+    nextMenu() {
+      this.$refs.slider.goToPage(this.$refs.slider.getNextPage())
+    },
+    prevMenu() {
+      this.$refs.slider.goToPage(this.$refs.slider.getPreviousPage())
+    },
+    url(image) {
+      return require(`../assets/img/home/slider/slide-${image}.jpg`)
+    },
+  },
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style lang="scss" scoped>
+.ms-header {
+  position: relative;
+  min-height: 50vh;
+  text-align: left;
+
+  .carousel-container {
+    padding: 0;
+    width: 100%;
+    position: relative;
+    margin: auto;
+
+    .carousel {
+      width: 100%;
+      margin: auto;
+    }
+
+    span {
+      cursor: pointer;
+      top: 50%;
+      transform: translateY(-70%);
+      position: absolute;
+      z-index: 2;
+
+      svg {
+        height: 16px;
+      }
+
+      &.prev {
+        left: 20px;
+      }
+
+      &.next {
+        right: 20px;
+      }
+    }
+  }
+
+  .VueCarousel-slide {
+    position: relative;
+
+    .slide-image {
+      position: relative;
+      height: 100%;
+      overflow: hidden;
+
+      img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+
+    &.VueCarousel-slide-active img {
+      animation: scale 10000ms ease-in-out forwards;
+    }
+
+    .slide-content {
+      position: absolute;
+      width: 50%;
+      top: 50%;
+      left: 16%;
+      transform: translateY(-50%);
+      text-align: left;
+      color: #fff !important;
+
+      h1 {
+        font-weight: 700;
+        color: #fff;
+        line-height: 1.4;
+        margin: 0;
+        width: 75%;
+      }
+    }
+  }
+}
+
+@keyframes scale {
+  to {
+    transform: scale(1.3);
+  }
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.35);
+}
+
+.overlay.header-gradient {
+  background: rgb(0, 146, 63);
+  /* background: linear-gradient(90deg, rgba(0, 146, 63, 1) 0%, rgba(0, 146, 63, 1) 34%, rgba(255, 255, 255, 0) 56%); */
+  background: linear-gradient(
+    135deg,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(0, 0, 0, 0.2) 54%,
+    rgba(255, 255, 255, 0) 76%
+  );
+}
+
+.owl-nav {
+  position: absolute;
+  top: 40%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  width: 94%;
+  left: 3%;
+  right: 3%;
+}
+
+.owl-nav button:focus {
+  outline: none !important;
+  box-shadow: none !important;
+}
+
+.owl-dots {
   align-items: center;
-  text-align: center;
+  position: absolute;
+  top: 80%;
+  left: 50%;
+  transform: translateX(-50%);
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.owl-dots .owl-dot {
+  height: 14px;
+  width: 14px;
+  border-radius: 100px;
+  background: #ffffff74 !important;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.owl-dots .owl-dot.active {
+  background: #fcb320 !important;
+  transition: background 0.3s ease-out;
 }
 
-.links {
-  padding-top: 15px;
+.owl-dot:not(:last-child) {
+  margin-right: 7px;
+}
+
+.header-carousel .owl-dots {
+  left: unset;
+  right: 5%;
+}
+
+.our-partners .owl-dots .owl-dot {
+  background: #001a0b30 !important;
+}
+
+.our-partners .owl-dots .owl-dot.active {
+  background: #fcb320 !important;
+}
+
+.ms-header--ctas {
+  padding: 0;
+}
+
+.ms-header--ctas__container {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.ms-header--ctas p:only-child {
+  font-weight: 600;
+  line-height: 1.6;
+}
+
+.ms-header--cta {
+  width: calc(33.33% - 20px);
+  position: relative;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: left;
+  align-items: flex-start;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  background: #fcb320;
+  min-height: 180px;
+  position: relative;
+  top: 0;
+  transition: all 0.3s ease-out;
+}
+
+.ms-header--cta a {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.ms-header--cta.inverse {
+  background: #00923f;
+}
+
+/* .ms-header--cta img {
+  margin-bottom: 10px;
+  height: 32px;
+}
+
+.ms-header--cta p {
+  font-weight: 500;
+  line-height: 1.4;
+  color: #fff;
+  font-size: 1rem;
+  opacity: 1;
+} */
+
+.ms-header--cta img {
+  margin-bottom: 10px;
+  height: 32px;
+  text-align: right;
+}
+
+.ms-header--cta h6 {
+  font-weight: 600;
+  line-height: 1.6;
+  color: #fff;
+  font-size: 0.875rem;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+}
+
+.ms-header--cta p {
+  font-weight: 400;
+  line-height: 1.6;
+  color: #fff;
+  font-size: 0.875rem;
+  opacity: 1;
+}
+
+.ms-header--cta:hover {
+  background: #001a0b;
+  transition: all 0.3s ease-out;
+  top: -10px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.6);
 }
 </style>
