67b07919d343ca6dbf4fba5c265d349a450a369c
tobiadeniji <mygit-key>
2020-11-18 18:20:50 -0500

    deployment --dry-run --debug  test

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+projectName=$PROJECT_NAME
+chartName=$PROJECT_NAME
+
+echo $GPG_KEY | gpg --batch --yes --passphrase-fd 0 ssh_key.gpg && chmod 700 ssh_key
+
+ssh-keyscan -H bitbucket.org >> "$HOME/.ssh/known_hosts"
+
+GIT_SSH_COMMAND='ssh -i ssh_key' git clone -b gh-pages git@bitbucket.org:noApp/helm-charts.git
+
+chart=$( ls -t helm-charts/$chartName/ | head -n 1 )
+
+helm inspect values helm-charts/$chartName/$chart
+
+echo $GCP_KEY > helm/gcp_key.json
+ 
+gcloud auth activate-service-account --key-file=helm/gcp_key.json
+
+if [ $BITBUCKET_BRANCH = "piping" ] ; then
+  gcloud container clusters get-credentials nqlb-prod --region europe-west3 --project nqlb-207716
+  helm upgrade -i $projectName helm-charts/$chartName/$chart --namespace default -f helm/prod.yaml --dry-run --debug
+fi
+
+#EOF
