91ad62426d9bdbe58beeba1790e4ff2816b813e8
Ismail Omodara <omodara145@gmail.com>
2020-12-10 23:29:07 +0100

    Google analytics one render

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,12 +66,6 @@ export default {
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&family=Poppins:wght@600&display=swap', media: 'print', onload: `${this.onload=null}${this.media='all'}`}
     ],
     script: [
-      // Google Analytics
-      {
-        innerHTML: "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-178017824-1', 'auto');ga('send', 'pageview');",
-        type: 'text/javascript',
-        defer: true
-      },
 
       // Global site tag (gtag.js) - Google Analytics
       {
